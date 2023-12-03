0d9d2e93d0b05c79503511e282f090f1f495fb88
dakotachatt <dvchatt@gmail.com>
2023-12-03 15:19:59 -0700

    Dec 2 pt I and II

--- src/day-2/a.ts
+++ src/day-2/a.ts
@@ -1,9 +1,48 @@
 import { readData } from '../utils';
 import chalk from 'chalk';
 
+type PullResult = {
+  number: number;
+  color: string;
+}
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let gamesPossibleSum = 0;
+  const maxNumRed = 12;
+  const maxNumGreen = 13;
+  const maxNumBlue = 14;
+
+  for(let i = 0; i < data.length; i++) {
+    const trimmedLine = data[i].trim();
+    const gameResults = trimmedLine.slice(data[i].indexOf(':') + 1);
+    const allPulls = gameResults.split(/[;,]+/)
+    let isGamePossible = true;
+
+    for(let j = 0; j < allPulls.length; j++) {
+      const result = allPulls[j].trim().split(' ');
+
+      if(result[1] === 'red' && Number.parseInt(result[0]) > maxNumRed) {
+        isGamePossible = false;
+
+        break;
+      } else if (result[1] === 'green' && Number.parseInt(result[0]) > maxNumGreen) {
+        isGamePossible = false;
+
+        break;
+      } else if (result[1] === 'blue' && Number.parseInt(result[0]) > maxNumBlue) {
+        isGamePossible = false;
+
+        break;
+      }
+    }
+
+    if(isGamePossible) {
+      gamesPossibleSum += (i + 1);
+    }
+  }
+
+  return gamesPossibleSum;
 }
 
 // don't change below this line
--- src/day-2/b.ts
+++ src/day-2/b.ts
@@ -1,9 +1,39 @@
 import { readData } from '../utils';
 import chalk from 'chalk';
 
+type ResultObject = {
+  color: string,
+  numberOfCubes: number
+}
+
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let powersSum = 0;
+
+  for(let i = 0; i < data.length; i++) {
+    const trimmedLine = data[i].trim();
+    const gameResults = trimmedLine.slice(data[i].indexOf(':') + 1);
+    const allPulls = gameResults.split(/[;,]+/)
+    const resultObjectArray: ResultObject[] = [];
+
+    for(let j = 0; j < allPulls.length; j++) {
+      const result = allPulls[j].trim().split(' ');
+      console.log('Hand:', result);
+
+      resultObjectArray.push({
+        color: result[1],
+        numberOfCubes: Number.parseInt(result[0])
+      });
+    }
+
+    const redCubes = Math.max(...resultObjectArray.filter((entry) => entry.color === 'red').map(entry => entry.numberOfCubes));
+    const greenCubes = Math.max(...resultObjectArray.filter((entry) => entry.color === 'green').map(entry => entry.numberOfCubes));
+    const blueCubes = Math.max(...resultObjectArray.filter((entry) => entry.color === 'blue').map(entry => entry.numberOfCubes));
+
+    powersSum += redCubes * greenCubes * blueCubes;
+  }
+
+  return powersSum;
 }
 
 // don't change below this line
